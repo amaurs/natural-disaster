3a2fee4d2d07ac9bc0700395875d614a1e4c3c80
amaurs <amaury.gtz@gmail.com>
2017-06-23 12:15:15 -0400

    The images are lazily downloaded, and then ingested to db.

--- flood/mapper/data/scene.py
+++ flood/mapper/data/scene.py
@@ -7,11 +7,25 @@ from __future__ import unicode_literals
 
 import rasterio
 
+from flood.configuration import SETTINGS
 from flood.mapper.base import BaseData
 from flood.persistence.database.connection import Scene
+from flood.util import get_base_name, create_file_name, check_if_file_exists, get_basename_of_file
 
-from flood.util import get_base_name
-from flood.configuration import SETTINGS
+
+def maybe_download(filename, work_directory, url):
+    print filename
+    print work_directory
+    
+    image_file = create_file_name(work_directory, filename)
+    
+    print image_file
+    if check_if_file_exists(image_file):
+        print 'The file already exists!'
+    else:
+        print 'Does not exist, must download it.'
+        import urllib
+        urllib.urlretrieve (url, image_file)
 
 class Data(BaseData):
     def __init__(self, image_path, year, url):
@@ -20,15 +34,20 @@ class Data(BaseData):
         self.temporary = getattr(SETTINGS, 'TEMP_FOLDER')
         self.destiny = getattr(SETTINGS, 'DATA_FOLDER')
         self.url = url
+        self.name = get_base_name(self.image_path)
+        filename = get_basename_of_file(self.image_path)
+        maybe_download(filename, self.temporary, self.url)
+        print url
+        
         with rasterio.open(self.image_path) as dataset:
             self.width = dataset.width
             self.height = dataset.height
         
     def get_database_object(self):
-        name = get_base_name(self.image_path)
         
         
-        return Scene(name=name,
+        
+        return Scene(name=self.name,
                      width=self.width,
                      height=self.height,
                      year=self.year,
@@ -40,4 +59,3 @@ class Data(BaseData):
     def get_files(self):
         return [self.image_path]
 
-        
