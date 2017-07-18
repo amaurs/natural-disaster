981550ae6578a107d76792ec714571dc92cbf694
amaurs <amaury.gtz@gmail.com>
2017-07-18 13:20:46 -0400

    Added a GET method that serves an images including the prediction.

--- flood/web/app.py
+++ flood/web/app.py
@@ -4,6 +4,9 @@ Created on Jun 23, 2017
 @author: agutierrez
 '''
 
+import shutil
+from subprocess import PIPE
+import subprocess
 import uuid
 
 from PIL import ImageOps, Image
@@ -89,6 +92,60 @@ def images():
     
     '''
     response = jsonify({'image_name' : image_name})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route("/floods/predict", methods=['GET'])
+def predict():
+    random_scene = get_random_scene().first()
+    name = random_scene['name']
+    path = random_scene['path']
+    scene_id = random_scene['pk_id']
+    filename = '%s.tif' % create_file_name(path, name)
+    print filename
+    with rasterio.open(filename) as src:
+        r, g, b, a = src.read()
+        scene_width = src.width
+        scene_height = src.height
+    image = numpy.array([r,g,b,a])
+    size_width = 322
+    size_height = 322
+    size = size_height
+    offset_x = numpy.random.randint(size_width, scene_width) - size_width
+    offset_y = numpy.random.randint(size_height, scene_height) - size_height
+    rgbArray = numpy.zeros((size,size,3), 'uint8')
+    rgbArray[..., 0] = r[offset_y:offset_y + size_height,offset_x:offset_x + size_width]
+    rgbArray[..., 1] = g[offset_y:offset_y + size_height,offset_x:offset_x + size_width]
+    rgbArray[..., 2] = b[offset_y:offset_y + size_height,offset_x:offset_x + size_width]
+    im = Image.fromarray(rgbArray)
+    training_data = getattr(SETTINGS, 'TRAINING_DATA_FOLDER')
+    image_name = '%s.jpg' % uuid.uuid4()
+    static_url = '/floods/training/%s' % image_name
+    image_path = create_file_name(training_data, image_name)
+    my_sample = sample.Data(image_name, offset_x, offset_y, size_width, size_height, scene_id, static_url)
+    ingest_sample(my_sample)
+    im.save(image_path)
+    
+    tensorflow_folder = '%s:/tf_files' % getattr(SETTINGS, 'TENSORFLOW_FOLDER')
+    
+    print tensorflow_folder
+    
+    destination = create_file_name(getattr(SETTINGS, 'TENSORFLOW_FOLDER'), 'unseen')
+    print destination
+    
+    
+    shutil.copy(image_path, destination)
+    
+    command_array = ['/usr/local/bin/docker','run', '--rm', '--volume', tensorflow_folder, '--workdir', '/tf_files', 'tensorflow/tensorflow:1.1.0', 'python', 'label_image.py', '/tf_files/unseen/%s' % image_name]
+    
+    command_string = ' '.join(command_array)
+    print command_string
+    
+    docker_response = subprocess.Popen(command_string, stdout=PIPE, shell=True).stdout.read()
+    
+    
+    response = jsonify({'image_name' : image_name, 'prediction' : docker_response.strip().split('\n')})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
