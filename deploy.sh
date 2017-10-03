0e51cccf7ff893f759539cf5d45ef5617b08fdac
amaurs <amaury.gtz@gmail.com>
2017-10-03 15:43:35 -0500

    added files to gitignore..

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-OPTIONS="-avzC --exclude-from exclude.txt"
-TRANSPORT='ssh -p 22'
-CONNECTION='amau@45.33.126.223'
-REMOTE_PATH='/home/amau/disaster'
-
-rsync $OPTIONS -e "$TRANSPORT" . $CONNECTION:$REMOTE_PATH
